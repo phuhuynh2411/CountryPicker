070dd3c15ab1490357c0cc5a391a736dbc504dc7
Phu Huynh <phu@filesoft.tech>
2020-03-09 22:01:59 +0700

    * Completed country picker

--- CountryPicker/ContentView.swift
+++ CountryPicker/ContentView.swift
@@ -9,8 +9,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var countrySelection = 0
+    
+    let countries = ["Albania", "Algeria", "Andorra", "Angola", "Antigua and Barbuda", "Argentina"]
+    let flags = ["flag-of-Albania", "flag-of-Algeria", "flag-of-Andorra", "flag-of-Angola", "flag-of-Antigua", "flag-of-Argentina"]
+    
     var body: some View {
-        Text("Hello, World!")
+       
+        VStack {
+            Picker(selection: $countrySelection, label: Text("")) {
+                ForEach(0..<countries.count) { (index) in
+                    HStack(spacing: 20) {
+                        Image(self.flags[index])
+                            .resizable()
+                            .frame(width: 20, height: 20, alignment: .leading)
+                        
+                        Text(self.countries[index]).tag(index)
+                        Spacer()
+                    }
+                }
+            }.labelsHidden()
+            
+            Text("Picked country: \(countries[countrySelection])")
+        }
     }
 }
 
